622594c9c77c53b4631ccbb9f94a7e74dfa731ab
Miles <miles.moscara@gmail.com>
2019-05-21 22:12:15 -0400

    disabled bad code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,34 +19,34 @@ var profiles = [
 // of the differences between each profile from the input profile scores and puts it next to the name of the profile. I could not
 // figure out how to associate the lowest number with its adjacent, corresponding name of the matching profile to display it.
 
-function friendFinder(latestProfile) {
-	var store = [];
-	var store2 = [];
-	var store3 = [];
-	var sum = latestProfile.reduce((partial_sum, a) => partial_sum + a, 0);
+// function friendFinder(latestProfile) {
+// 	var store = [];
+// 	var store2 = [];
+// 	var store3 = [];
+// 	var sum = latestProfile.reduce((partial_sum, a) => partial_sum + a, 0);
 
-	for (let i = 0; i < profiles.length - 1; i++) {
-		store.push(profiles[i].scores);
-		store.push(profiles[i].name);
-	}
-	for (let i = 0; i < store.length; i++) {
-		if (i % 2 === 0) {
-			store2.push(store[i].reduce((partial_sum, a) => partial_sum + a, 0));
-		} else {
-			store2.push(store[i]);
-		}
-	}
-	for (let i = 0; i < store2.length; i++) {
-		if (i % 2 === 0) {
-			store3.push(Math.abs(store2[i] - sum));
-		} else {
-			store3.push(store2[i]);
-		}
-	}
-	console.log(store3);
-}
+// 	for (let i = 0; i < profiles.length - 1; i++) {
+// 		store.push(profiles[i].scores);
+// 		store.push(profiles[i].name);
+// 	}
+// 	for (let i = 0; i < store.length; i++) {
+// 		if (i % 2 === 0) {
+// 			store2.push(store[i].reduce((partial_sum, a) => partial_sum + a, 0));
+// 		} else {
+// 			store2.push(store[i]);
+// 		}
+// 	}
+// 	for (let i = 0; i < store2.length; i++) {
+// 		if (i % 2 === 0) {
+// 			store3.push(Math.abs(store2[i] - sum));
+// 		} else {
+// 			store3.push(store2[i]);
+// 		}
+// 	}
+// 	console.log(store3);
+// }
 
-friendFinder(latestProfile.scores);
+// friendFinder(latestProfile.scores);
 
 /////////////////////////////////////////////////////////////////
 
